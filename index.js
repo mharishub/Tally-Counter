19c024f63a8f293a93f775f6d98adb917786e3c1
M haris <mharis1240@gmail.com>
2021-05-21 11:28:18 +0500

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+var count=0;
+function inc()
+{
+    count++;
+    document.getElementById("dis").innerHTML=count;
+}
+function res()
+{
+    count=0;
+    document.getElementById("dis").innerHTML=count;
+}
